593d41e69b53bac49290e2a95b6f9cee50df6eea
Nicolas <novares.x@gmail.com>
2020-06-11 19:42:52 +0200

    Add page skeleton

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Elm dashboard for Firefox and Chromium
 
-This is an Elm project to learn the basis by making a beautiful dashboard for your favorite webbrowser.
+This is an [Elm](https://elm-lang.org/) project to learn the basis by making a beautiful dashboard for your favorite webbrowser.
 
-An overview is available here: https://nicolasguilloux.github.io/elm-workshop/
+A good example is the extension [Momemtum](https://momentumdash.com/). This will be our base. The goal is to build an extension for Chromium or Firefox to customize the "New tab" page with a beautiful random background, the weather, some links and of course the centrale clock.
+
+Thanks to Github Action and Pages, we can build the assets of our master branch and push it to the `gh-pages` branch to make it available from a link: https://nicolasguilloux.github.io/elm-workshop/
+
+
+## Installation
+
+The only thing you need is `yarn` installed on your machine and up to date. This is the only dependency as it will install everything we need, especially Elm.
+
+
+## Features
+
+This is a list of all features that we will bring to our extension.
+
+ - A clock
+ - A random welcome message
+ - The weather
+
+## Mentions
+
+The workshop was designed by [Nicolas Guilloux](https://github.com/NicolasGuilloux/). Feel free to copy, tweak, comment as you want.
+
+The wallpapers are taken from the [Pexels website](https://www.pexels.com/search/nature%20wallpaper/).
--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,10 +1,14 @@
 module Main exposing (..)
 
 import Browser exposing (Document)
-import Html exposing (Html, div, text)
+import Html exposing (Attribute, Html, div, h1, text)
+import Html.Attributes exposing (class, style)
+
 
 
 -- Describes how the program should be initialized
+
+
 main : Platform.Program Flags Model Msg
 main =
     Browser.document
@@ -15,38 +19,124 @@ main =
         }
 
 
+
 -- Defines the input data (Flags), the container where we store our variables (Model), and the various
 -- messages trigerred by the view or actions to let use edit our current state of the Model
 
+
 type alias Flags =
-    {}
+    { firstname : String
+    }
+
 
 type alias Model =
-    {}
+    { firstname : String
+    }
+
 
 type Msg
     = MsgNoOp
 
 
+
 -- Describes how the application should be initialized
-init : Flags -> (Model, Cmd Msg)
-init _ =
-    ( {}, Cmd.none )
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( { firstname = flags.firstname
+      }
+    , Cmd.none
+    )
+
+
 
 -- Describes the various actions to do when a message is triggered
-update : Msg -> Model -> (Model, Cmd Msg)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         MsgNoOp ->
             ( model, Cmd.none )
 
+
+
 -- Describes how the view should be rendered
+
+
 view : Model -> Document Msg
 view model =
-    { title = "Elm Dashboard" 
-    , body = 
-        [ div []
-            [ text "It's alive!"
+    { title = "Elm Dashboard"
+    , body =
+        [ div [ class "wrapper", backgroundImageStyle ]
+            [ topLeftWidget model
+            , topRightWidget model
+            , centerWidget model
+            , bottomLeftWidget model
+            , bottomRightWidget model
             ]
         ]
     }
+
+
+backgroundImageStyle : Attribute Msg
+backgroundImageStyle =
+    let
+        bgUrl =
+            \filename -> "url('images/backgrounds/" ++ filename ++ "')"
+    in
+    style "background-image" (bgUrl "background_1.jpg")
+
+
+
+-- Center widget
+
+
+centerWidget : Model -> Html Msg
+centerWidget model =
+    div [ class "center-widget" ]
+        [ h1 []
+            [ text ("Hello " ++ model.firstname)
+            ]
+        ]
+
+
+
+-- Top Left width
+
+
+topLeftWidget : Model -> Html Msg
+topLeftWidget _ =
+    div [ class "top-left-widget" ]
+        []
+
+
+
+-- Top Right width
+
+
+topRightWidget : Model -> Html Msg
+topRightWidget _ =
+    div [ class "top-right-widget" ]
+        []
+
+
+
+-- Bottom Left width
+
+
+bottomLeftWidget : Model -> Html Msg
+bottomLeftWidget _ =
+    div [ class "bottom-left-widget" ]
+        []
+
+
+
+-- Bottom Right width
+
+
+bottomRightWidget : Model -> Html Msg
+bottomRightWidget _ =
+    div [ class "bottom-right-widget" ]
+        []
